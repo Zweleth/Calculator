234b17e793463312da582814ee2e3d95885f985c
Zweleth <zwebeko@gmail.com>
2022-11-24 12:35:22 +0200

    Day 2 update

--- js/code.js
+++ js/code.js
@@ -1,15 +1,85 @@
 let current = document.getElementById("current");
 let previous = document.getElementById("prev");
+let allowComa = true;
+let allowOp = false;
+let clrPrev = false;
+let lastOp = "" ;
 
-let calculate = (num) => {
+let addToSum = (num) => {
     if (eval(previous.value) == current.value) {
-        if (isFinite(num)) {
+        if (isFinite(num) || (num==".")) {
             clr()
         }
 
         
     }
     current.value += num ;
+    lastOp += num;
+    allowOp = true;
+    clrPrev = false;
+}
+
+let addToSumComa = (num) => {
+    if (allowComa === true) {
+        if (eval(previous.value) == current.value) {
+            if (num==".") {
+                clr()
+            }
+    
+            
+        }
+        current.value += num ;
+        allowComa = false;
+        clrPrev = false;
+    }
+}
+
+let addToSumDiv = (num) => {
+    if (allowOp === true) {
+        current.value += num;
+        lastOp = "";
+        lastOp += num;
+        allowComa = true ;
+        allowOp = false;
+        clrPrev = false;
+    }
+    
+}
+
+let addToSumPlus = (num) => {
+    if (allowOp === true) {
+        current.value += num;
+        allowComa = true ;
+        allowOp = false;
+        clrPrev = false;
+        lastOp = "";
+        lastOp += num;
+    }
+    
+}
+
+let addToSumMin = (num) => {
+    if (allowOp === true) {
+        current.value += num;
+        allowComa = true ;
+        allowOp = false;
+        clrPrev = false;
+        lastOp = "";
+        lastOp += num;
+    }
+    
+}
+
+let addToSumMul = (num) => {
+    if (allowOp === true) {
+        current.value += num;
+        allowComa = true ;
+        allowOp = false;
+        clrPrev = false;
+        lastOp = "";
+        lastOp += num;
+    }
+    
 }
 
 let Ans = () => {
@@ -23,7 +93,15 @@ let Ans = () => {
 }
 
 function clr() {
+    if (clrPrev === true) {
+        previous.value=previous.value.slice(0,-0)
+    }
     current.value=current.value.slice(0,-0);
+    allowComa = true ;
+    clrPrev = true;
+    clrPrev === true;
+    
+
 }
 
 function del() {
@@ -31,11 +109,19 @@ function del() {
 }
 
 function answer() {
-    
+    if (eval(previous.value) == current.value) {
+        
+        current.value = eval(current.value+=lastOp);
+        previous.value = current.value+=lastOp;
+    }
     previous.value = current.value;
     Ans();
     let operation = eval(current.value)
     current.value = operation
+    allowComa = true ;
+    clrPrev = false;
+    
+
 }
 
 function sqrt() {
